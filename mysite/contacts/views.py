7ebcfb5fc0a790ab0ab739209273c28b287d315d
Israel Macedo da Silva <raelmacedodasilva@gmail.com>
2024-12-24 11:31:23 -0300

    feat: criação de novo app contact e criação do form usando api django

---
+++ mysite/contacts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect,   HttpResponse
+from django.urls import reverse
+
+
+from .forms import NameForm
+
+def get_name(request):
+    if request.method == 'POST':
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['your_name']
+            return HttpResponseRedirect(reverse('contacts:thanks', args=(name,)))
+        
+    else:
+        form = NameForm()   
+    
+    return render(request, 'contacts/name.html', {'form': form})
+
+
+def thanks(request, name):
+    
+    return HttpResponse(f'Obrigado { name }!')
+# Create your views here.
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin_site.urls),
-    path("polls/", include("polls.urls"))
+    path("polls/", include("polls.urls")),
+    path("contacts/", include("contacts.urls"))
 ]
